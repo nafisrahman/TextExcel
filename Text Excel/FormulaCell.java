068819288246f0c9bff67c774a6116fcd7761759
Nafis Rahman <42381892+nafisrahman@users.noreply.github.com>
2019-09-26 17:23:24 -0400

    Update FormulaCell.java

--- Text Excel/FormulaCell.java
+++ Text Excel/FormulaCell.java
@@ -1,14 +1,3 @@
-/**********************************************************
- * Assignment: (Formula Cell)
- *
- * Author: (Nafis Rahman)
- *
- * Description: (It takes a formula and give the answer)
- *
- * Academic Integrity: I pledge that this program represents my own work. I
- * received help from () in designing and debugging
- * my program.!
- **********************************************************/
 package textExcel5;
 
 public class FormulaCell extends Cell
